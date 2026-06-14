058984954f295df1f63a302ab95a82eb3efebd56
Shaikh Al Nahian <sheikhalnahian@gmail.com>
2026-06-14 11:59:32 +0600

    matches table is created

--- query.sql
+++ query.sql
@@ -11,3 +11,18 @@ CREATE TABLE Users (
     constraint uk_user_email unique(email),
     constraint check_user_role check(role in ('Ticket Manager','Football Fan'))
 );
+
+--create matches table
+CREATE TABLE Matches (
+    match_id SERIAL,
+    fixture VARCHAR(100),
+    tournament_category VARCHAR(100),
+    base_ticket_price NUMERIC(10,2),
+    match_status VARCHAR(30),
+
+    CONSTRAINT pk_matches PRIMARY KEY (match_id),
+    CONSTRAINT chk_base_ticket_price
+        CHECK (base_ticket_price >= 0),
+    CONSTRAINT chk_match_status
+        CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
