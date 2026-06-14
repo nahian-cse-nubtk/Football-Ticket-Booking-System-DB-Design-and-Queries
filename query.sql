425b9352335e4aadee767e967b193f5696ed8e32
Shaikh Al Nahian <sheikhalnahian@gmail.com>
2026-06-14 15:17:41 +0600

    query for displaying comprehensive list of all users are created

--- query.sql
+++ query.sql
@@ -120,3 +120,13 @@ FROM
     bookings b
     INNER JOIN users u ON b.user_id = u.user_id
     INNER JOIN matches m ON b.match_id = m.match_id;
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM
+    users u
+    LEFT JOIN bookings b ON b.user_id = u.user_id
